3fcad9833ec797ae71138f0c042aec95b5ca72fa
0xSendo <142055962+0xSendo@users.noreply.github.com>
2026-02-04 17:46:46 +0800

    Update App.jsx

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -2,10 +2,39 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import StudentDashboard from "./pages/dashboards/StudentDashboard";
+import EmployerDashboard from "./pages/dashboards/EmployerDashboard";
+import AdminDashboard from "./pages/dashboards/AdminDashboard";
+import RequireAuth from "./components/RequireAuth";
+
 
 function App() {
   return (
     <Routes>
+      <Route
+        path="/dashboard/student"
+        element={
+          <RequireAuth>
+            <StudentDashboard />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/dashboard/employer"
+        element={
+          <RequireAuth>
+            <EmployerDashboard />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/dashboard/admin"
+        element={
+          <RequireAuth>
+            <AdminDashboard />
+          </RequireAuth>
+        }
+      />
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
