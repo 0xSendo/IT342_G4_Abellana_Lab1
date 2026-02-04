213c67435352e31c346e8df5bb9415d7b32d1d3d
0xSendo <142055962+0xSendo@users.noreply.github.com>
2026-02-04 17:44:17 +0800

    Update AdminDashboard.jsx

--- Web/src/pages/dashboards/AdminDashboard.jsx
+++ Web/src/pages/dashboards/AdminDashboard.jsx
@@ -1,1 +1,21 @@
+import DashboardLayout from "../../components/DashboardLayout";
 
+export default function AdminDashboard() {
+  return (
+    <DashboardLayout title="Admin Dashboard">
+      <section className="card">
+        <h3>User Management</h3>
+        <ul>
+          <li>Student: 120</li>
+          <li>Employers: 15</li>
+        </ul>
+      </section>
+
+      <section className="card">
+        <h3>Internship Monitoring</h3>
+        <p>Active Postings: 25</p>
+        <p>Reported Listings: 0</p>
+      </section>
+    </DashboardLayout>
+  );
+}
