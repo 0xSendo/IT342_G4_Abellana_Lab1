89768f78a657e182b3c22f4e09fe25f4c7b11c1a
0xSendo <142055962+0xSendo@users.noreply.github.com>
2026-02-10 00:01:03 +0800

    Update InternmatchApplication.java

--- Backend/src/main/java/com/internmatch/internmatch/InternmatchApplication.java
+++ Backend/src/main/java/com/internmatch/internmatch/InternmatchApplication.java
@@ -2,8 +2,10 @@ package com.internmatch.internmatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.internmatch.internmatch.repository")
 public class InternmatchApplication {
 
 	public static void main(String[] args) {
